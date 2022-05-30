3cf1b864826221b34ac1486c11efb18919919b00
陈思敏 <chensiming379@163.com>
2022-05-30 20:15:16 +0800

    基于VUE和springbot实现音乐网站

---
+++ music-client/src/assets/data/swiper.js
@@ -0,0 +1,20 @@
+// 轮播图
+const swiperList = [{
+  picImg: require('@/assets/img/swiper/民族1.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/民族5.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/民族2.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/民族7.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/民族3.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/民族6.jpg')
+}, {
+  picImg: require('@/assets/img/swiper/民族4.jpg')
+}]
+
+export {
+  swiperList
+}
